ce653f3a7cb68fe58f889e3a76eb9f64b191ffd8
HollowedOutMods <112557613+HollowedOutMods@users.noreply.github.com>
2023-10-31 13:17:09 +0000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-error(".gg/amethysts")
+error("Mayhem mode is gone until further notice, please touch some grass while we wait for pc executors to be fixed. Perhaps make some friends or get a life. -Hallow/Ame")
